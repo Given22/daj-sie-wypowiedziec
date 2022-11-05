7966d887f8bd78247a133ab058c9bfa99836973f
Damian S <damian.sroczynski.22@gmail.com>
2022-11-05 01:37:09 +0100

    up

--- App.js
+++ App.js
@@ -17,15 +17,22 @@ import GameStack from "./components/GameStack"
 const Tab = createBottomTabNavigator()
 
 export default function App() {
-<<<<<<< Updated upstream
+
+  const [loaded] = useFonts({
+   Itim: require('./assets/fonts/Itim-Regular.ttf'),
+  });
+
+
+  if (!loaded) {
+    return null;
+  }
+
 	return (
-		// <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-		//   <Text>Settings!</Text>
-		// </View>
 		<NavigationContainer>
 			<Tab.Navigator
 				tabBarOptions={{
 					showLabel: false,
+          keyboardHidesTabBar: true,
 				}}
 				screenOptions={({ route }) => ({
 					headerShown: false,
@@ -38,7 +45,7 @@ export default function App() {
 						borderTopRightRadius: 20,
 					},
 				})}
-				initialRouteName='Game'>
+				initialRouteName='Home'>
 				<Tab.Screen
 					options={{ headerShown: false }}
 					name='First'
@@ -56,36 +63,35 @@ export default function App() {
 				/>
 			</Tab.Navigator>
 		</NavigationContainer>
-	)
-=======
-  return (
-    <NavigationContainer>
+	)}
+
+//   return (
+//     <NavigationContainer>
       
-      <Tab.Navigator 
-        tabBarOptions={{
-          showLabel: false,
-          }}
-          screenOptions={({ route }) => ({
-            headerShown: false,
-            tabBarStyle: {
-              height: 60,
-              backgroundColor: '#A4C3B2',
-              position: 'absolute',
-              borderTopWidth: 0,
-              borderTopLeftRadius: 20,
-              borderTopRightRadius: 20,
-          },
-        })}
-        initialRouteName="Game"
-      >
-        <Tab.Screen options={{headerShown: false}}  name="First" component={FirstScreen} />
-        <Tab.Screen options={{headerShown: false}}  name="Home" component={HomeScreen} />
-        <Tab.Screen options={{headerShown: false}}  name="Game" component={GameScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
->>>>>>> Stashed changes
-}
+//       <Tab.Navigator 
+//         tabBarOptions={{
+//           showLabel: false,
+//           }}
+//           screenOptions={({ route }) => ({
+//             headerShown: false,
+//             tabBarStyle: {
+//               height: 60,
+//               backgroundColor: '#A4C3B2',
+//               position: 'absolute',
+//               borderTopWidth: 0,
+//               borderTopLeftRadius: 20,
+//               borderTopRightRadius: 20,
+//           },
+//         })}
+//         initialRouteName="Game"
+//       >
+//         <Tab.Screen options={{headerShown: false}}  name="First" component={FirstScreen} />
+//         <Tab.Screen options={{headerShown: false}}  name="Home" component={HomeScreen} />
+//         <Tab.Screen options={{headerShown: false}}  name="Game" component={GameScreen} />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 
 const styles = StyleSheet.create({
 	container: {
